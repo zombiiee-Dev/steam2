cd2b78693112fb4e8d9dc237b51b5da4f5b34bfa
zombiiee-Dev <phung2012lk@gmail.com>
2023-07-20 14:37:27 +0700

    sua ban hoan chinh

--- script.js
+++ script.js
@@ -1,193 +1,133 @@
 
-const queryString = window.location.search;
-
-let urlParam = new URLSearchParams(queryString);
-
-let genres = urlParam.get("genres") || "";
-let search = urlParam.get("q") || "";
-let page = urlParam.get("page") || 1;
-
-console.log(search);
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let search = urlParams.get("q") || "";
+let genres = urlParams.get("genres") || "";
+let page = urlParams.get("page") || 1;
 
 
-const getAllGame = async(search,genres,page)=>{
-    try {
-        let urlGame = `https://steam-api-mass.onrender.com/games?q=${search}&page=${page}`;
-        if (genres) {
-            urlGame += `&genres=${genres}`
-          }
-        const response = await fetch(urlGame);
-        if(response.ok) {
-            const data = await response.json();
-            console.log("check",data);
-            return data;
-        } 
-    } catch (error) {
-        console.log(error);
+
+// Set lại tham số 'page' trong URL và chuyển trang
+
+const getAllGame = async()=>{
+  try {
+    let urlGame = `https://steam-api-mass.onrender.com/games?&q=${search}&page=${page}`;
+    if(genres) urlGame+=`&genres=${genres}`;
+    const response = await fetch(urlGame);
+    if(response.ok){
+      const data = await response.json();
+      console.log("game",data);
+      return data;
     }
- }
- const promiseGetGame = getAllGame();
-const display = document.querySelector(".game-wrapper");
+  } catch (error) {
+    console.log(error);
+  }
+}
 const renderAllGame = async()=>{
+  try {
+    const display = document.querySelector(".game-wrapper");
     display.innerHTML="";
-    const response = await getAllGame(search,genres,page);
+    const response = await getAllGame();
+    
     response.data.forEach((game)=>{
-        const price = game.price;
-        const element = document.createElement("a");
-        element.setAttribute("class","setImage");
-        element.setAttribute("href",`detail.html?id=${game.appid}`);
-        element.innerHTML=`<div class="cover">
-        <img src=${game.header_image}>
-        <div class="game-info" id=${game.appid}>
-          <p>${price===0?"free to play":price+"$"}</p>
-          <p>${game.name}</p>
-        </div>
-      </div>`;
-      display.appendChild(element);
+      const element = document.createElement("a");
+      element.setAttribute("href",`detail.html?id=${game.appid}`);
+      element.classList.add("setImage");
+      // const price = game.price;
+      element.innerHTML=`<div class="cover">
+      <img src=${game.header_image}>
+      <div class="game-info" id=${game.appid}>
+        <p>${game.name}</p>
+        <p>${(game.price)===0?"free to play":(game.price)+"$"}</p>
+      </div>
+    </div>`;
+    display.appendChild(element);
     })
+  } catch (error) {
+    console.log(error);
+  }
 }
 renderAllGame();
-const pageNumber = document.querySelector(".pageNumber");
-const buttonPrevious = document.querySelector(".previousPage").addEventListener("click",(e)=>{
-    previousPage();
-});
-const buttonNext = document.querySelector(".nextPage").addEventListener("click",(e)=>{
-    nextPage();
-  }
-);
-
-const previousPage = () => {
-   
-    if (page > 1) {
-      page--;
-      pageNumber.textContent=page;
-      getAllGame(search, genres, page);
-      renderAllGame();
-    }
-  };
-const nextPage = () => {
-   
-    page++;
-    pageNumber.textContent=page;
-    getAllGame(search, genres, page);
-    renderAllGame();
-  };
-
-const urlMenu = "https://steam-api-dot-cs-platform-306304.et.r.appspot.com/genres?&page=1&limit=7";
-const getMenu = async()=>{
-    try {
-        const response = await fetch(urlMenu);
-        if(response.ok){
-            const data = await response.json();
-            console.log("oday",data);
-            return data;
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const renderMenu = async()=>{
-    try {
-        const genresMenu = document.querySelector(".genres");
-       
-        const response = await getMenu();
-        response.data.forEach((menu)=>{
-            const x = document.createElement("a");
-            x.classList.add("type-games");
-           if (genres === menu.name) {x.classList.add("active");}
-            x.textContent= menu.name;
-            x.setAttribute("href",`index.html?genres=${menu.name}&page=${page}`);
-            genresMenu.appendChild(x);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
-renderMenu();
-
-
-
-const urlGenres = `https://steam-api-dot-cs-platform-306304.et.r.appspot.com/games?genres=${genres}&page=${page}`;
-
-const getGenres = async()=>{
-try {
+let urlGenres = `https://steam-api-mass.onrender.com/genres?&page=${page}&limit=7`;
+const getGenresList = async()=>{
+  try {
     const response = await fetch(urlGenres);
-    if(response.ok){
-        const data = await response.json();
-        console.log("123",data);
-        return data;
+    if(response.ok) {
+      const data = await response.json();
+      console.log("genres",data);
+      return data;
     }
-} catch (error) {
+  } catch (error) {
     console.log(error);
+  }
 }
-}
+
 const renderGenres = async()=>{
-    try {
-        display.innerHTML="";
-        const response = await getGenres(genres,page);
-        response.data.forEach((game)=>{
-            const price = game.price;
-            const element = document.createElement("a");
-        element.setAttribute("class","setImage");
-        element.setAttribute("href",`detail.html?id=${game.appid}`);
-        element.innerHTML=`<div class="cover">
-        <img src=${game.header_image}>
-        <div class="game-info" id=${game.appid}>
-          <p>${price===0?"free to play":price+"$"}</p>
-          <p>${game.name}</p>
-        </div>
-      </div>`;
-      display.appendChild(element);
-        })
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const genresMenu = document.querySelector(".genres");
+    const response = await getGenresList();
+    const pageData = response.page;
+    console.log(pageData);
+    response.data.forEach((menu)=>{
+      const a = document.createElement("a");
+      a.classList.add("type-games");
+      if(genres === menu.name) {a.classList.add("active")};
+      a.textContent=menu.name;
+      a.setAttribute("href",`index.html?genres=${menu.name}`);
+      genresMenu.appendChild(a);
+    })
+  } catch (error) {
+    console.log(error);
+  }
 }
 renderGenres();
 
-// const getSearch = async(search)=>{
-//     try {
-//      let urlSearch = `https://steam-api-mass.onrender.com/games?q=${search}`;
-//      const response = await fetch(urlSearch);
-//        if (response.ok) {
-//          const data = await response.json();
-//          // console.log("dataSearch",data);
-//          return data;
-     
-//     }} catch (error) {
-//      console.log("renderSearch",error);
-//     }
-//    }
-   
-   const renderSearch = async (search) => {
-     try {
-       const dataSearch = await getAllGame(search);
-       if(dataSearch) {
-         window.location.href = `index.html?q=${search}`;
-         }
-       } catch (error) {
-       console.log(error);
-     }
-   };
-   
-   const searchForm = document.querySelector("#searchForm");   
-   const searchInput = document.querySelector(".search");  
+
+const form = document.querySelector(".search");
+const searchInput = document.querySelector("#searchForm");
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  const inputValue = searchInput.value;
+  renderSearch(inputValue);
   
-   searchInput.addEventListener("submit",(e)=>{
-       e.preventDefault();
-       let inputSearch = e.target.searchForm.value;
-       console.log(inputSearch);
-       renderSearch(inputSearch);
-       })
-   const searchImg = document.querySelector("#search-img");
-     searchImg.addEventListener("click",(e)=>{
-     e.preventDefault();
-     let inputSearch = searchForm.value;
-     console.log(inputSearch);
-     renderSearch(inputSearch);
-   })
+})
+const searchImg = document.querySelector("#search-img");
+searchImg.addEventListener("click",(e)=>{
+  e.preventDefault();
+  const inputValue = searchInput.value;
+  renderSearch(inputValue);
+})
+const renderSearch = async (search) => {
+  try {
+    const dataSearch = await getAllGame(search);
+    if(dataSearch) {
+      window.location.href = `index.html?q=${search}`;
+      }
+    } catch (error) {
+    console.log(error);
+  }
+};
+const pageNumber = document.querySelector(".page-number");
+
+document.querySelector(".previous-page").addEventListener("click",()=>{
+  if(page>1)page--;
+  pageNumber.textContent = page;
+  let urlParams = new URLSearchParams(window.location.search);
+  urlParams.set('page', page);
+  window.history.replaceState({}, '', `index.html?${urlParams.toString()}`);
+  getAllGame(page);
+  renderAllGame();
+   }
+  );
+document.querySelector(".next-page").addEventListener("click",()=>{
+  page++;
+  pageNumber.textContent = page;
+  let urlParams = new URLSearchParams(window.location.search);
+  urlParams.set('page', page);
+  window.history.replaceState({}, '', `index.html?${urlParams.toString()}`);
+  getAllGame(page);
+  renderAllGame();
+})
 
 
-  
-  
